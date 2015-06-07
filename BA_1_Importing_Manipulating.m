5a8b57cbf02b0c53b4820941fa68e2b83c467cf1
Davmil <davidmilew@gmail.com>
2015-06-07 13:41:51 +0200

    Importing ect.

--- BA_1_Importing_Manipulating.m
+++ BA_1_Importing_Manipulating.m
@@ -4,36 +4,56 @@ cd C:\Users\David\Documents\Bachelorarbeit\main;
 % cd D:\Bachelorarbeit\main;
 format short g
 
-%% % 1a) Import datasets as tables (via 'Import Data'-Button)
-% 1b) Manipulate datasets
+%% % 1a) Import dataset datorig
 
-% 
-% opts.ID = strrep(opts.ID,'"','');
-% opts.Expiry = strrep(opts.Expiry,'"','');
-% 
-% optPrices.ID = strrep(optPrices.ID,'"','');
-% optPrices.Date = strrep(optPrices.Date,'"','');
-% 
-% daxVals.Date = strrep(daxVals.Date,'"','');
-% 
-% cohortParams.Date = strrep(cohortParams.Date,'"','');
-% cohortParams.Expiry = strrep(cohortParams.Expiry,'"','');
-% 
-% addObs0913.ID = strrep(addObs0913.ID,'"','');
-% addObs0913.Date = strrep(addObs0913.Date,'"','');
-% addObs0609.ID = strrep(addObs0609.ID,'"','');
-% addObs0609.Date = strrep(addObs0609.Date,'"','');
-% 
-% daxVals = [daxVals table(datenum(daxVals.Date))];
-% daxVals.Properties.VariableNames{3} = 'DateFormat';
-% 
-% cohortParams = [cohortParams table(datenum(cohortParams.Expiry))];
-% cohortParams.Properties.VariableNames{5} = 'ExpiryDateFormat';
+filename = 'C:\Users\David\Documents\Bachelorarbeit\data\_dat_orig.txt';
+delimiter = '\t';
+startRow = 2;
 
+formatSpec = '%s%f%s%s%f%f%f%s%f%f%f%f%[^\n\r]';
 
+fileID = fopen(filename,'r');
 
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+fclose(fileID);
+
+datorig = table(dataArray{1:end-1}, 'VariableNames', {'Date','Option_Price','Bid','Ask','Volume','Open_Interest','Strike','Expiry','DAX','EONIA_matched','Time_to_Maturity','IsCall'});
+
+clearvars filename delimiter startRow formatSpec fileID dataArray ans;
+
+%% Manipulate
+datorig.Bid = []; datorig.Ask = [];
+
+%% Seperate in Call and Put
+
+callopt = datorig(datorig.IsCall==1,:);
+putopt = datorig(datorig.IsCall==0,:);
+
+%% Insert ID variable into datorig table
+load callPrices
+load putPrices
+% Calls
+callopt = sortrows(callopt,'Expiry','ascend');
+callopt = sortrows(callopt,'Strike','ascend');
+callopt = sortrows(callopt,'Date','ascend');
+callPrices = sortrows(callPrices,'Strike','ascend');
+callPrices = sortrows(callPrices,'Date','ascend');
+callopt = [callopt callPrices.ID];
+callopt.Properties.VariableNames{11} = 'ID';
+
+%% Puts
+putopt = sortrows(putopt,'Expiry','ascend');
+putopt = sortrows(putopt,'Strike','ascend');
+putopt = sortrows(putopt,'Date','ascend');
+putPrices = sortrows(putPrices,'Strike','ascend');
+putPrices = sortrows(putPrices,'Date','ascend');
+putopt = [putopt putPrices.ID];
+putopt.Properties.VariableNames{11} = 'ID';
 %% % 2a) Save the data to an .mat-file
-
+% save callopt callopt;
+% save putopt putopt;
+% save datorig datorig;
 % save opts opts
 % save optprices optPrices
 % save daxvals daxVals
@@ -43,7 +63,8 @@ format short g
 % clear all
 
 
-%% % 2b) Load .mat-datasets
+%% % 2b) Load .mat-datasets ##############################################
+% ########################################################################
 load opts 
 load optprices 
 load daxvals 
@@ -52,9 +73,12 @@ load addobs0913
 load addobs1314
 load callPrices
 load putPrices
+load callopt
+load putopt
 
 calls = opts(strcmp(opts.IsCall,'true'),:);
 puts = opts(strcmp(opts.IsCall,'false'),:);
+
 %%
 % Working days generally:
 
@@ -73,77 +97,86 @@ putPrices.workingdays2mat = round(putPrices.workingdays2mat);
 % length(opts(strcmp(opts.IsCall,'true'),2).Expiry) 
 % length(opts(strcmp(opts.IsCall,'false'),2).Expiry)
 
-%% % Optionen im Datensatz mit einer Laufzeit von weniger als zwei Wochen bzw. 10 Handelstagen
-
-% Calls: insgesamt 56 Calls
-B = unique(callPrices.ID);
-j=1;
-for i = 1:length(calls.ID)
-    opt = callPrices(strcmp(callPrices.ID,B(i,1)),6);
-    if (max(opt.workingdays2mat)<10)
-       cless(j,1) = B(i,1);
-%        cless_cont(j,1) = max(opt.workingdays2mat);
-       j = j+1;
-    else 
-        continue
-    end      
-end
-
-% Puts: insgesamt 61 puts
-B = unique(putPrices.ID);
-j=1;
-for i = 1:length(puts.ID)
-    opt = putPrices(strcmp(putPrices.ID,B(i,1)),6);
-    if (max(opt.workingdays2mat)<10)
-       pless(j,1) = B(i,1);
-%        pless_cont(j,1) = max(opt.workingdays2mat);
-       j = j+1;
-    else 
-        continue
-    end      
-end
-
-clearvars j i B opt
-%% % Entferne Optionen (Puts und Calls) mit einer Laufzeit von weniger als 10 Handelstagen
-for i = 1:length(cless)     % Calls
-    rows_to_remove = any(strcmp(callPrices.ID,cless(i)), 2);
-    rows_to_remove2 = any(strcmp(calls.ID,cless(i)), 2);
-    rows_to_remove3 = any(strcmp(opts.ID,cless(i)), 2);
-    rows_to_remove4 = any(strcmp(optPrices.ID,cless(i)), 2);
-    rows_to_remove5 = any(strcmp(addObs0609.ID,cless(i)), 2);
-    rows_to_remove6 = any(strcmp(addObs0913.ID,cless(i)), 2);
-    
-    callPrices(rows_to_remove,:) = [];
-    calls(rows_to_remove2,:) = [];
-    opts(rows_to_remove3,:) = [];
-    optPrices(rows_to_remove4,:) = [];
-    addObs0609(rows_to_remove5,:) = [];
-    addObs0913(rows_to_remove6,:) = [];
-end
-clearvars rows_to_remove* i
-
-for i = 1:length(pless)     % Puts
-    rows_to_remove = any(strcmp(putPrices.ID,pless(i)), 2);
-    rows_to_remove2 = any(strcmp(puts.ID,pless(i)), 2);
-    rows_to_remove3 = any(strcmp(opts.ID,pless(i)), 2);
-    rows_to_remove4 = any(strcmp(optPrices.ID,pless(i)), 2);
-    rows_to_remove5 = any(strcmp(addObs0609.ID,pless(i)), 2);
-    rows_to_remove6 = any(strcmp(addObs0913.ID,pless(i)), 2);
-    
-    putPrices(rows_to_remove,:) = [];
-    puts(rows_to_remove2,:) = [];
-    opts(rows_to_remove3,:) = [];
-    optPrices(rows_to_remove4,:) = [];
-    addObs0609(rows_to_remove5,:) = [];
-    addObs0913(rows_to_remove6,:) = [];
-end
-clearvars rows_to_remove* i cless pless 
+% %% % Optionen im Datensatz mit einer Laufzeit von weniger als zwei Wochen bzw. 10 Handelstagen
+% 
+% % Calls: insgesamt 56 Calls
+% B = unique(callPrices.ID);
+% j=1;
+% for i = 1:length(calls.ID)
+%     opt = callPrices(strcmp(callPrices.ID,B(i,1)),6);
+%     if (max(opt.workingdays2mat)<10)
+%        cless(j,1) = B(i,1);
+% %        cless_cont(j,1) = max(opt.workingdays2mat);
+%        j = j+1;
+%     else 
+%         continue
+%     end      
+% end
+% 
+% % Puts: insgesamt 61 puts
+% B = unique(putPrices.ID);
+% j=1;
+% for i = 1:length(puts.ID)
+%     opt = putPrices(strcmp(putPrices.ID,B(i,1)),6);
+%     if (max(opt.workingdays2mat)<10)
+%        pless(j,1) = B(i,1);
+% %        pless_cont(j,1) = max(opt.workingdays2mat);
+%        j = j+1;
+%     else 
+%         continue
+%     end      
+% end
+% 
+% clearvars j i B opt
+% %% % Entferne Optionen (Puts und Calls) mit einer Laufzeit von weniger als 10 Handelstagen
+% for i = 1:length(cless)     % Calls
+%     rows_to_remove = any(strcmp(callPrices.ID,cless(i)), 2);
+%     rows_to_remove2 = any(strcmp(calls.ID,cless(i)), 2);
+%     rows_to_remove3 = any(strcmp(opts.ID,cless(i)), 2);
+%     rows_to_remove4 = any(strcmp(optPrices.ID,cless(i)), 2);
+%     rows_to_remove5 = any(strcmp(addObs0609.ID,cless(i)), 2);
+%     rows_to_remove6 = any(strcmp(addObs0913.ID,cless(i)), 2);
+%     
+%     callPrices(rows_to_remove,:) = [];
+%     calls(rows_to_remove2,:) = [];
+%     opts(rows_to_remove3,:) = [];
+%     optPrices(rows_to_remove4,:) = [];
+%     addObs0609(rows_to_remove5,:) = [];
+%     addObs0913(rows_to_remove6,:) = [];
+% end
+% clearvars rows_to_remove* i
+% 
+% for i = 1:length(pless)     % Puts
+%     rows_to_remove = any(strcmp(putPrices.ID,pless(i)), 2);
+%     rows_to_remove2 = any(strcmp(puts.ID,pless(i)), 2);
+%     rows_to_remove3 = any(strcmp(opts.ID,pless(i)), 2);
+%     rows_to_remove4 = any(strcmp(optPrices.ID,pless(i)), 2);
+%     rows_to_remove5 = any(strcmp(addObs0609.ID,pless(i)), 2);
+%     rows_to_remove6 = any(strcmp(addObs0913.ID,pless(i)), 2);
+%     
+%     putPrices(rows_to_remove,:) = [];
+%     puts(rows_to_remove2,:) = [];
+%     opts(rows_to_remove3,:) = [];
+%     optPrices(rows_to_remove4,:) = [];
+%     addObs0609(rows_to_remove5,:) = [];
+%     addObs0913(rows_to_remove6,:) = [];
+% end
+% 
+% clearvars rows_to_remove* i cless pless 
 
 % Nun haben wir Optionsscheine im Datensatz, die eine Laufzeit von
 % mindestens 2 Wochen bzw. 10 Handelstagen aufweisen!!!
 
+%%
+callPrices = [callPrices table(callopt.DAX)];
+callPrices.Properties.VariableNames{7} = 'DAX';
 
+putPrices = [putPrices table(putopt.DAX)];
+putPrices.Properties.VariableNames{7} = 'DAX';
 
-
-
-
+%% Renaming
+putopt.Properties.VariableNames{8} = 'EONIA';
+callopt.Properties.VariableNames{8} = 'EONIA';
+cohortParams.Properties.VariableNames{3} = 'EONIA';
+putopt.Properties.VariableNames{2} = 'Price';
+callopt.Properties.VariableNames{2} = 'Price';
