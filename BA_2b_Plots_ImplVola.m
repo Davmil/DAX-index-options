3c0da8dc098805bb01bf8e75fbeacb90b26b7d16
Davmil <davidmilew@gmail.com>
2015-07-21 10:14:07 +0200

    Update

--- BA_2b_Plots_ImplVola.m
+++ BA_2b_Plots_ImplVola.m
@@ -7,12 +7,12 @@
 
 %% // on January 17,2008
 
-smile1 = callopt(strcmp(callopt.Expiry,'2008-01-18') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 1 day to Mat.
-smile2 = callopt(strcmp(callopt.Expiry,'2008-02-15') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 1 month
-smile3 = callopt(strcmp(callopt.Expiry,'2008-03-20') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 2 months
-smile4 = callopt(strcmp(callopt.Expiry,'2008-06-20') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 6 months
-smile5 = callopt(strcmp(callopt.Expiry,'2008-09-19') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 9 months
-smile6 = callopt(strcmp(callopt.Expiry,'2008-12-19') & strcmp(callopt.Date,'2008-01-17'),[5 12]); % 12 months
+smile1 = mydatc(strcmp(mydatc.Expiry,'2008-01-18') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 1 day to Mat.
+smile2 = mydatc(strcmp(mydatc.Expiry,'2008-02-15') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 1 month
+smile3 = mydatc(strcmp(mydatc.Expiry,'2008-03-20') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 2 months
+smile4 = mydatc(strcmp(mydatc.Expiry,'2008-06-20') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 6 months
+smile5 = mydatc(strcmp(mydatc.Expiry,'2008-09-19') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 9 months
+smile6 = mydatc(strcmp(mydatc.Expiry,'2008-12-19') & strcmp(mydatc.Date,'2008-01-17'),[5 12]); % 12 months
     % 2008-01-21 : schwerste Rückgänge seit dem 11.Sep. 2001 an der Börse
     
 daxVals.DAX(257) % Dax Stand am 17. Jan. 2008 (für ATM Vola) 
@@ -59,12 +59,12 @@ clearvars smile*
 
 %% // on January 18,2010 
 
-smile1 = callopt(strcmp(callopt.Expiry,'2010-01-15') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 1 day to Mat.
-smile2 = callopt(strcmp(callopt.Expiry,'2010-02-19') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 1 month
-smile3 = callopt(strcmp(callopt.Expiry,'2010-03-19') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 2 months
-smile4 = callopt(strcmp(callopt.Expiry,'2010-07-16') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 6 months
-smile5 = callopt(strcmp(callopt.Expiry,'2010-09-17') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 9 months
-smile6 = callopt(strcmp(callopt.Expiry,'2010-12-17') & strcmp(callopt.Date,'2010-01-14'),[5 12]); % 12 months
+smile1 = mydatc(strcmp(mydatc.Expiry,'2010-01-15') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 1 day to Mat.
+smile2 = mydatc(strcmp(mydatc.Expiry,'2010-02-19') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 1 month
+smile3 = mydatc(strcmp(mydatc.Expiry,'2010-03-19') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 2 months
+smile4 = mydatc(strcmp(mydatc.Expiry,'2010-07-16') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 6 months
+smile5 = mydatc(strcmp(mydatc.Expiry,'2010-09-17') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 9 months
+smile6 = mydatc(strcmp(mydatc.Expiry,'2010-12-17') & strcmp(mydatc.Date,'2010-01-14'),[5 12]); % 12 months
 
     
 daxVals.DAX(897) % Dax Stand am 18.01.2010 (für ATM Vola) 
@@ -117,17 +117,27 @@ clearvars smile*
 
 %% // on January 17,2008
 
-volsrf = callopt(strcmp(callopt.Date,'2008-01-17'),[5 9 12]); % Maturity, ImplVola
+volsrf = mydatc(strcmp(mydatc.Date,'2008-01-17'),[5 9 12 7]); % Maturity, ImplVola
 figure('position',[100 100 1200 800])
+
 srfce = VolSurf2(volsrf.Time_to_Maturity, volsrf.Strike, volsrf.ImplVola);
 savefig('figures/Vola_surface2_01_17_2008.fig');
 
+scatter3(volsrf.Time_to_Maturity, volsrf.Strike, volsrf.ImplVola);
+
 %% // on January 15,2010 
 
-volsrf = callopt(strcmp(callopt.Date,'2010-01-15'),[5 9 12]); % Maturity, ImplVola
+volsrf = mydatc(strcmp(mydatc.Date,'2010-01-15'),[5 9 12 7]); % Maturity, ImplVola
 figure('position',[100 100 1200 800])
 srfce = VolSurf2(volsrf.Time_to_Maturity, volsrf.Strike, volsrf.ImplVola);
 savefig('figures/Vola_surface2_01_15_2010.fig');
+%%
+scatter3(volsrf.Strike, volsrf.Time_to_Maturity, volsrf.ImplVola,'filled');
+title('Implied Volatility Surface');
+xlabel('Strike Level');
+ylabel('Time to Maturity');
+zlabel('Implied Volatility');
+set(gca);
 
 % ======================================================================================
 % ============= (3D) ImplVola - Maturity - Moneyness  =====================
@@ -135,7 +145,7 @@ savefig('figures/Vola_surface2_01_15_2010.fig');
 
 %% // on January 17,2008
 
-volsrf = callopt(strcmp(callopt.Date,'2008-01-17'),[9 12]); % Maturity, ImplVola
+volsrf = mydatc(strcmp(mydatc.Date,'2008-01-17'),[9 12]); % Maturity, ImplVola
 volsrf = [ volsrf callPrices(strcmp(callPrices.Date,'2008-01-17'),8) ];
 figure('position',[100 100 1200 800])
 srfce = VolSurf(volsrf.Time_to_Maturity, volsrf.mnyness, volsrf.ImplVola);
@@ -143,7 +153,7 @@ savefig('figures/Vola_surface_01_17_2008.fig');
 
 %% // on January 15,2010 
 
-volsrf = callopt(strcmp(callopt.Date,'2010-01-15'),[9 12]); % Maturity, ImplVola
+volsrf = mydatc(strcmp(mydatc.Date,'2010-01-15'),[9 12]); % Maturity, ImplVola
 volsrf = [ volsrf callPrices(strcmp(callPrices.Date,'2010-01-15'),8) ];
 figure('position',[100 100 1200 800])
 srfce = VolSurf(volsrf.Time_to_Maturity, volsrf.mnyness, volsrf.ImplVola);
@@ -151,7 +161,7 @@ savefig('figures/Vola_surface_01_15_2010.fig');
 
 %% // on December 2,2013 '2013-12-02'
 
-volsrf = callopt(strcmp(callopt.Date,'2013-12-02'),[9 12]); % Maturity, ImplVola
+volsrf = mydatc(strcmp(mydatc.Date,'2013-12-02'),[9 12]); % Maturity, ImplVola
 volsrf = [ volsrf callPrices(strcmp(callPrices.Date,'2013-12-02'),8) ];
 figure('position',[100 100 1200 800])
 srfce = VolSurf(volsrf.Time_to_Maturity, volsrf.mnyness, volsrf.ImplVola);
